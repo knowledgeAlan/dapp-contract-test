f113e45a30f72d6a2f7e4fe92ca1019162ef75a4
zzm <zzm12990@gmail.com>
2023-10-06 18:08:26 +0800

    array

---
+++ .history/contract/Array_20231006122051.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.18;
+
+
+contract Array {
+    constructor() {
+        
+    }
+
+
+    uint[] public arr;
+    uint[] public arr2 = [1,2,3];
+
+    uint[10] public myFixedSizeArr;
+
+
+    function get(uint i) public view returns (uint) {
+        return arr[i];
+    }
+
+    function getArr() public view returns (uint[] memory){
+        return arr;
+    }
+
+
+    function push(uint i) public pure{
+        arr.push(i);
+    }
+
+
+
+
+}
